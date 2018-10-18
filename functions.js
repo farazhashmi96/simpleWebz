563a0ed1a5a3250f799d6880089ac13c26b3f1c2
farazhashmi96 <farazhashmi96@yahoo.com>
2018-10-18 19:18:25 +0500

    Script Updated

--- functions.js
+++ functions.js
@@ -15,3 +15,20 @@ function changeColor(){
 function letsHide(){
   document.getElementById("picx").style.display= "none";
 }
+function Fahrenheit_to_Celsius(temp){
+var getTemp = temp;
+var statementText = "Weather Calculate";
+var Fahrenheit_to_Celsius = (((getTemp-32)*5)/9);
+var result = getTemp+" Fahrenheit into Celsius = "+Fahrenheit_to_Celsius+" Celsius.";
+console.log(result);
+document.getElementById("tempe").innerHTML = result;
+}
+function Celsius_to_Fahrenheit(temp){
+var getTemp = temp;
+var statementText = "Weather Calculate";
+var Celsius_to_Fahrenheit = (((getTemp*9)/5)+32);
+var result = getTemp+" Celsius into Fahrenheit = "+Celsius_to_Fahrenheit+" Fahrenheit.";
+console.log(result);
+document.getElementById("tempe").innerHTML = result;
+}
+
